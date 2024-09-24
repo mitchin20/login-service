3da423f191cc295226aeb9ec80e1613bab2c64e4
Mit Chin <mitchin10@gmail.com>
2024-09-24 17:19:34 -0400

    Exclude password from User

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -23,6 +23,11 @@ def authenticate_user(db: Session, email: str, password: str):
         return None
     return user
 
+# exclude fields
+def to_dict(self, exclude=None):
+    exclude = exclude or []
+    return {key: value for key, value in self.__dict__.items() if key not in exclude}
+
 # Login service to handle login logic
 def login_user(db: Session, email: str, password: str):
     user = authenticate_user(db, email, password)
@@ -32,6 +37,8 @@ def login_user(db: Session, email: str, password: str):
             detail="Incorrect Email or Password",
             headers={"WWW-Authenticate": "Bearer"}
         )
+    
+    user_without_password = to_dict(user, exclude=["password"])
         
     # Create JWT token for the user
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTE)
@@ -41,6 +48,7 @@ def login_user(db: Session, email: str, password: str):
         )
     
     return {
+        "user": user_without_password,
         "access_token": access_token,
         "token_type": "Bearer"
     }
