7a51235d40fa96d7836565ba5be496183bfc94c5
Mit Chin <mitchin10@gmail.com>
2024-09-26 02:22:46 -0400

    Add lifespan handler

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.routers import auth_router
 from app.routers import verify_token
 from app.middlewares.token_validation_middleware import TokenValidationMiddleware
+from app.db.postgres_database import database
 
-app = FastAPI()
+# Lifespan handler: Manages the lifecycle of the app, handling startup and shutdown
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup db connection
+    await database.connect()
+
+    yield
+    
+    # shutdown db connection
+    await database.disconnect()
+    
+app = FastAPI(lifespan=lifespan)
 
 # root route
 @app.get("/")
